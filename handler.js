e2768a58a277a3325c9f152322dba92425538245
Edward Woodcock <info@edwardwoodcock.com>
2015-12-11 11:46:59 +0000

    Remove redundant param

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ var action = require('./app/index.js');
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
-  action.run(event, context, function(error, result) {
+  action.run(event, function(error, result) {
     return context.done(error, result);
   });
 };
